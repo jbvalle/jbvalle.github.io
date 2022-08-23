5c0b3198e7f1dece0cd61bf3bf88392bc3e15168
John Bryan Valle <jbvalle.jv@gmail.com>
2022-08-23 21:34:56 +0100

    upd

--- get_ext.sh
+++ get_ext.sh
@@ -3,6 +3,6 @@
 #ext_ip=$(lynx -dump $ext_url)
 MAKE_PATH=/home/strayker/Coding/jbvalle.github.io
 
-curl ipinfo.io/ip > ext.txt && hostname -i >> ext.txt && cd $MAKE_PATH && make update
+curl ipinfo.io/ip > ext.txt && echo "" >> ext.txt && hostname -i >> ext.txt && cd $MAKE_PATH && make update
 
 
